82fd20fe511cefbab4fecbe994b8db56d5f35193
漠中洲 <61797908+mozhongzhou@users.noreply.github.com>
2024-01-12 01:37:46 +0800

    Add files via upload

---
+++ Config.lua
@@ -0,0 +1,43 @@
+return {
+	Title = "太吾神功",
+	Version = 281474976710657,
+	FrontendPlugins = {
+		[1] = "TaiWuShenGong.dll",
+	},
+	Author = "yyf",
+	Description = "提供三款龙傲天功法 \n神·太吾长拳\n神·太吾纵跃功\n神·太吾内功\n24/1/12更新",
+	Cover = "fengmian.png",
+	Source = 0,
+	FileId = 3136844670,
+	GameVersion = "0.0.68.39",
+	WorkshopCover = "fengmian.png",
+	TagList = {
+		[1] = "Modifications",
+		[2] = "Compatible Mods",
+	},
+	Visibility = 0,
+	DefaultSettings = { },
+	UpdateLogList = {
+		[1] = {
+			Timestamp = 1704972447,
+			LogList = {
+				[1] = "",
+			},
+		},
+		[2] = {
+			Timestamp = 1704991698,
+			LogList = {
+				[1] = "新增三个神一功",
+			},
+		},
+		[3] = {
+			Timestamp = 1704992724,
+			LogList = {
+				[1] = "",
+			},
+		},
+	},
+	DetailImageList = {
+		[1] = "屏幕截图 2024-01-11 212614.png",
+	},
+}
